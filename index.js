916d0de10f00aa0661fab15ad1f0effcc42bbef1
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-22 15:28:28 -0600

    added basic isntructions

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-// 
+// to run: node index.js
+let myVariable = "hello world";
+const other = 100;
+let isReal = true;
+let person = {
+    name: "John",
+    age: 25
+};
+person.name = "Luck";
+let something = [123,"red", true];
 
-console.log('Hello world!');
+function myfunction(name) {
+    console.log("hello " + name);
+
+}
+
+myfunction(123);

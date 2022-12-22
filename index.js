bf6c709b92ee11e2e6d29e07719af0f6a7b6c7aa
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-12-22 13:42:21 -0600

    changes in code

--- index.js
+++ index.js
@@ -41,9 +41,21 @@ myElement.setAttributeNode(myClass);
 document.body.appendChild(myElement);
 */
 
-let bouton = document.querySelector('#myButton');
+let bouton0 = document.querySelector('#myButton0');
+let bouton1 = document.querySelector('#myButton1');
 
-function addParagraph() {
+// function addParagraph(myText) {
+//     let myElement = document.createElement('p');
+//     let myText = document.createTextNode(myText);
+//     let myClass = document.createAttribute('class');
+//     myClass.value = 'someclass';
+//     myElement.appendChild(myText);
+//     myElement.setAttributeNode(myClass);
+//     document.body.appendChild(myElement);
+// }
+ReactDOM.render(<h1> </h1>, document.querySelector('#root'))
+
+bouton0.addEventListener('click', function addParagraph() {
     let myElement = document.createElement('p');
     let myText = document.createTextNode('hello');
     let myClass = document.createAttribute('class');
@@ -51,10 +63,30 @@ function addParagraph() {
     myElement.appendChild(myText);
     myElement.setAttributeNode(myClass);
     document.body.appendChild(myElement);
-}
+}, false);
 
+bouton1.addEventListener('click', function addParagraph() {
+    let myElement = document.createElement('p');
+    let myText = document.createTextNode('hola');
+    let myClass = document.createAttribute('class');
+    myClass.value = 'someclass';
+    myElement.appendChild(myText);
+    myElement.setAttributeNode(myClass);
+    document.body.appendChild(myElement);
+}, false);
+
+// function chooseText(){
+
+
+// }
+
+// bouton0.addEventListiner('click', chooseText);
+// bouton1.addEventListener('click', chooseText);
 // bouton.addEventListener('click', addParagraph);
-bouton.onclick = addParagraph;
+// if(bouton0)
+// bouton0.onclick = addParagraph('hello');
+// bouton1.onclick = addParagraph('goodbye');
+// bouton.onclick = changeText;
 
 list.appendChild(document.createElement('li'));
 list.children.item(2).textContent = 'Element 2';
